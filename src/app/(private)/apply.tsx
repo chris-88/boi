b9c80436f3b57fa918b38b079b46df5c38942a13
chris-88 <43267276+chris-88@users.noreply.github.com>
2023-12-05 16:26:40 +0000

    Remove unused imports and add WebView component

--- src/app/(private)/apply.tsx
+++ src/app/(private)/apply.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
 import WebView from "react-native-webview";
 // Context
 import { useTheme } from "../../context/ThemeContext";
--- src/app/(private)/services.tsx
+++ src/app/(private)/services.tsx
@@ -4,25 +4,23 @@ import { useTheme } from "../../context/ThemeContext";
 //Components
 import BOI_Header from "../../components/BOI_Header";
 import BOI_GetContent from "../../components/BOI_GetContent";
+import WebView from "react-native-webview";
 
 const Page = () => {
   const { theme } = useTheme();
 
   return (
-    <>
+    <View style={[styles.container, { backgroundColor: theme.background }]}>
       <BOI_Header title="Services" />
-      <View style={[styles.container, { backgroundColor: theme.background }]}>
-        <BOI_GetContent contentId="cms-test" />
-      </View>
-    </>
+      {/* <BOI_GetContent contentId="cms-test" /> */}
+      <WebView source={{ uri: "http://localhost:3000/" }} style={{ flex: 1 }} />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
